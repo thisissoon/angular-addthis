00f473ff1ff7bbfdab9705854d7f355313bd3584
Edward Opare-Aryee <ed@thisissoon.com>
2014-11-19 18:56:16 +0000

    remove example module

--- app/js/soon-example-app/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * @author   SOON_
- * @module   snExampleApp
- * @requires ngRoute {@link https://docs.angularjs.org/api/ngRoute}
- */
-angular.module("snExampleApp", ["ngRoute"]);
