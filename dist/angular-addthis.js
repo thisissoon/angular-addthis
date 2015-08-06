88c199327e975a0bef6ffc7b6f27f516531b416b
Edward Opare-Aryee <ed@thisissoon.com>
2015-08-06 16:20:26 +0100

    Release v0.2.0

--- dist/angular-addthis.js
+++ dist/angular-addthis.js
@@ -1,4 +1,4 @@
-/*! angular-addthis - v0.1.8 - 2015-07-02 */
+/*! angular-addthis - v0.2.0 - 2015-08-06 */
 "use strict";
 /**
  * AddThis widget directive, Re-renders addthis buttons as angular changes
@@ -50,9 +50,7 @@ angular.module("sn.addthis", [])
             restrict: "EAC",
             replace: false,
             scope: {
-                url: "@",
-                title: "@",
-                description: "@",
+                share: "="
             },
             link: function ($scope, $element) {
 
@@ -70,17 +68,6 @@ angular.module("sn.addthis", [])
                  * @type     {Object}
                  */
                 $scope.config = $window.addthis_config ? $window.addthis_config : {}; // jshint ignore:line
-
-                /**
-                 * {@link http://support.addthis.com/customer/portal/articles/1337996-the-addthis_share-variable}
-                 * @property share
-                 * @type     {Object}
-                 */
-                $scope.share = {
-                    url: $scope.url,
-                    title : $scope.title,
-                    description : $scope.description
-                };
 
                 /**
                  * Removes the stock addthis buttons
